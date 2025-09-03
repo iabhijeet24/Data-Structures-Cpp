e2b25dcb3a9125a7f69d02c073f7e25dfa4948e2
Abhijeet Singh <inabhijeet241107@gmail.com>
2025-09-04 00:11:54 +0530

    Arrays in Cpp

---
+++ Arrays/deletion.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, pos, val;
+    int arr[20] = {2, 4, 6, 8, 10, 12};
+    int size = 6;
+    
+    cout << "Size of array: "<< size << endl;
+
+  
+    cout << "Enter the Position:" << endl;
+    cin >> pos;
+    
+    for(i = (size-1); i >= pos; i--)
+    {
+        arr[i+1] = arr[i];
+    }
+    arr[pos] = val;
+    size++;
+    
+    cout << "Array after deletion: "; 
+    for (int i = 0; i < size; i++) 
+    {
+        cout << arr[i] << " "; 
+    }
+    cout << endl;
+
+    return 0;
+}
