fbcf156314a83084d6ad4a2185bb73981ed65d90
Abhijeet Singh <inabhijeet241107@gmail.com>
2025-09-06 11:02:18 +0530

    Arrays in cpp

--- Arrays/pointersarray.cpp
+++ Arrays/pointersarray.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 using namespace std;
 
-
-int main() {
-    
-    cout << "Hello, World!" << endl;
-    
+int main() 
+{
+  int arr[] = {1, 3, 5, 7, 9, 15};
+  int *p;
+  
+  p = arr;
+  
+  for(int i=0; i<=5; i++)
+  {
+      cout << arr[i] << endl;
+      cout << (p+i) << endl;
+      cout << &arr << endl;
+      cout << (p+i) << endl;
+      
+  }
     return 0;
 }
