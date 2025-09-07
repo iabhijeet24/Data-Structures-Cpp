c5170faec3f4bf7f057450d71be91a8fd5991883
Abhijeet Singh <inabhijeet241107@gmail.com>
2025-09-07 23:29:20 +0530

    Linked List in cpp

--- Arrays/2Darray.cpp
+++ Arrays/2Darray.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 using namespace std;
 
-
 int main() {
+    int i, j;
+    int row = 2, col = 2;
+    int arr[2][2];
+    cout << "Enter the elements in the matrix: \n" ;
+    for(i=0; i<2; i++)
+    {
+      for(j=0; j<2; j++)
+      {
+          cin >> arr[i][j];
+      }
+    }
     
-    cout << "Hello, World!" << endl;
     
+    cout << "DISPLAY OF ELEMENTS: \n" ;
+
+    for(i=0; i<2; i++)
+    {
+      for(j=0; j<2; j++)
+      {
+          cout << arr[i][j] << endl;
+      }
+    }
+
     return 0;
 }
