df6b36e415118764f98fd6bd91116599ef0558e5
Abhijeet Singh <inabhijeet241107@gmail.com>
2025-09-11 23:37:21 +0530

    Reversing in an array

--- Arrays/searching.cpp
+++ Arrays/searching.cpp
@@ -3,8 +3,32 @@ using namespace std;
 
 
 int main() {
+    bool found = false;
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int size = 10;
+    int key;
     
-    cout << "Hello, World!" << endl;
+    cout << "Enter the element: " << endl;
+    cin >> key;
+    
+    for(int i=0; i<size; i++)
+    {
+        if(arr[i] == key){
+            found = true;
+            break;
+        }
+        else{
+            found = false;
+        }
+    }
+    
+    if(found == true){
+        cout << "Element is present." << endl;
+    }
+    
+    else{
+        cout << "Not Present." << endl;
+    }
     
     return 0;
 }
