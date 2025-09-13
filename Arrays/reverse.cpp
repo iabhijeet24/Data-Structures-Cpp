14915a56172cbc021ecda3a3b44668602058dff6
Abhijeet Singh <inabhijeet241107@gmail.com>
2025-09-13 23:32:57 +0530

    Duplicate in array

--- Arrays/reverse.cpp
+++ Arrays/reverse.cpp
@@ -5,7 +5,7 @@ int main() {
     int arr[6] = {1, 2, 3, 4, 5, 6};
     int n = 6;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n/2; i++) {
         int temp = arr[i];
         arr[i] = arr[n-i-1];
         arr[n-i-1] = temp;
@@ -15,5 +15,5 @@ int main() {
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
-    return 0;
+return 0;
 }
